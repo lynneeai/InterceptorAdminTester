a5d24d23876db8ce6d605a4935befcbab78a6dad
lynneeai <lynneeai@hotmail.com>
2015-07-28 14:34:40 -0400

    ..

--- InterceptorAdminTester/InterceptorTester/Tests/AdminTests/AuthenticateTest.cs
+++ InterceptorAdminTester/InterceptorTester/Tests/AdminTests/AuthenticateTest.cs
@@ -37,20 +37,7 @@ namespace InterceptorTester.Tests.AdminTests
             Test authTest = new Test(authCall);
             AsyncContext.Run(async () => await new HTTPSCalls().runTest(authTest, HTTPOperation.POST));
             string statusCode = HTTPSCalls.result.Key.GetValue("StatusCode").ToString();
-<<<<<<< HEAD
-			try
-			{
-				sessionToken = JObject.Parse(HTTPSCalls.result.Value);
-            	Console.WriteLine(sessionToken.ToString());
-            	Assert.AreEqual("201", statusCode);
-			}
-			catch (Exception e) 
-			{
-				Console.WriteLine (statusCode);
-				Console.WriteLine (HTTPSCalls.result.Value);
-				Console.WriteLine (e.ToString ());
-			}
-=======
+
             Console.WriteLine(statusCode);
             if (!statusCode.Equals("201"))
             {
@@ -60,7 +47,7 @@ namespace InterceptorTester.Tests.AdminTests
             sessionToken = JObject.Parse(HTTPSCalls.result.Value);
             Console.WriteLine(sessionToken.ToString());
             Assert.AreEqual("201", statusCode);
->>>>>>> origin/master
+
         }
 
         [Test()]
